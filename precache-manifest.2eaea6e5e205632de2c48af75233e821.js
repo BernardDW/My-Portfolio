8e22d2749363c7180e17cd50263e194de6314c30
Berbaas <bdewet867@gmail.com>
2020-09-22 10:34:23 +0200

    Updates

---
+++ precache-manifest.2eaea6e5e205632de2c48af75233e821.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24db55416be1522d6e620598633debb8",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "53b12e38b8957b20ddbf",
+    "url": "/My-Portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1ae1a2860bfce0d59923",
+    "url": "/My-Portfolio/static/css/main.a7f02eb3.chunk.css"
+  },
+  {
+    "revision": "53b12e38b8957b20ddbf",
+    "url": "/My-Portfolio/static/js/2.d717284f.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/My-Portfolio/static/js/2.d717284f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae1a2860bfce0d59923",
+    "url": "/My-Portfolio/static/js/main.d974db50.chunk.js"
+  },
+  {
+    "revision": "bd26aa8a63ab866d9120",
+    "url": "/My-Portfolio/static/js/runtime-main.295a29ee.js"
+  },
+  {
+    "revision": "f52e3bed7497616d1a4400b4105da8a6",
+    "url": "/My-Portfolio/static/media/about.f52e3bed.jpg"
+  },
+  {
+    "revision": "00ed5050282d5b4e90e154007ceeb28c",
+    "url": "/My-Portfolio/static/media/intro.00ed5050.jpg"
+  },
+  {
+    "revision": "95ecb0493df2daceb059463fe5040dc3",
+    "url": "/My-Portfolio/static/media/rest.95ecb049.jpg"
+  }
+]);
